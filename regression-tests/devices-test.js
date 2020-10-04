aba06c6b47a30b5c89a2f8ca75f615133ce9e08b
akilaa.s1 <akilaa.s@thoughtworks.com>
2020-10-04 22:32:29 +0530

    add pixel 2 to devices test with no viewport error

--- regression-tests/devices-test.js
+++ regression-tests/devices-test.js
@@ -2,16 +2,37 @@ const puppeteer = require('puppeteer')
 const {expect} = require('chai');
 
 describe('Devices test', async () => {
-    it('iphone X  view', async () => {
-        const browser = await puppeteer.launch({headless: false});
-        const page = await browser.newPage();
-        const mobile = await puppeteer.devices['iphone X'];
-        await page.emulate(mobile);
+    let browser;
+    let page;
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 100
+        });
+        page = await browser.newPage();
         await page.goto("https://chromedevtools.github.io/devtools-protocol/");
+    })
+
+    it('iphone X  view', async () => {
+        const iphone = await puppeteer.devices['iphone X'];
+        await page.emulate(iphone);
+        await page.waitForTimeout(5000);
         const header = await page.$('h1');
         const title = await page.evaluate((header) => header.textContent, header);
         expect(title).equals('Chrome DevTools Protocol');
-        await browser.close();
-
     });
+
+    it('Pixel 2 view', async () => {
+        const pixel = await puppeteer.devices['Pixel 2'];
+        await page.emulate(pixel);
+        const header = await page.$('h1');
+        const title = await page.evaluate((header) => header.textContent, header);
+        expect(title).equals('Chrome DevTools Protocol');
+    });
+
+    afterEach(async () => {
+        await browser.close();
+    })
+
 });
