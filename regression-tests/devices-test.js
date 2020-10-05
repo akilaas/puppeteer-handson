c9af4238e8caf815992422093c2150f59fb35be1
akilaa.s1 <akilaa.s@thoughtworks.com>
2020-10-05 19:17:44 +0530

    fix devices test

--- regression-tests/devices-test.js
+++ regression-tests/devices-test.js
@@ -14,10 +14,39 @@ describe('Devices test', async () => {
         await page.goto("https://chromedevtools.github.io/devtools-protocol/");
     })
 
-    it('iphone X  view', async () => {
-        const iphone = await puppeteer.devices['iphone X'];
+    it('iphone X view', async () => {
+        const iphone = await puppeteer.devices['iPhone X'];
         await page.emulate(iphone);
-        await page.waitForTimeout(5000);
+        const header = await page.$('h1');
+        const title = await page.evaluate((header) => header.textContent, header);
+        expect(title).equals('Chrome DevTools Protocol');
+    });
+
+    it('Blackberry view', async () => {
+        //can emulate directly
+        await page.emulate({
+            name: 'Blackberry PlayBook',
+            userAgent:
+                'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML like Gecko) Version/7.2.1.0 Safari/536.2+',
+            viewport: {
+                width: 600,
+                height: 1024,
+                deviceScaleFactor: 1,
+                isMobile: true,
+                hasTouch: true,
+                isLandscape: false,
+            },
+        })
+        //can give viewport specs as well
+        // await page.setViewport({
+        //     width: 375,
+        //     width: 600,
+        //     height: 1024,
+        //     deviceScaleFactor: 1,
+        //     isMobile: true,
+        //     hasTouch: true,
+        //     isLandscape: false,
+        // })
         const header = await page.$('h1');
         const title = await page.evaluate((header) => header.textContent, header);
         expect(title).equals('Chrome DevTools Protocol');
